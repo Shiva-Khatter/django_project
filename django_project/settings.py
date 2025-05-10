90046fec73d7b0a3e6a78b21d9e7ce07ca434221
♡ Shiva Khatter ♡ <shiva.khatter@gmail.com>
2025-05-10 18:42:23 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-_n)a0g&g(nwj+afk2=e6#gc^ea
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*'] 
 
 # Application definition
 INSTALLED_APPS = [
