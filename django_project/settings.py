64f8e0bb84a367706ebc000db032d6af007a9b2a
♡ Shiva Khatter ♡ <shiva.khatter@gmail.com>
2025-05-10 18:42:09 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url  # Add this import for Heroku database configuration
+import dj_database_url  # Adding this import for Heroku database configuration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
